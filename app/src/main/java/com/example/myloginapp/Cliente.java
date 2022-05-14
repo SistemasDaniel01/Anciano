005b6fdd7e787ece443fd7dff5ee520e8c84eea7
danvalrl <79485807+danvalrl@users.noreply.github.com>
2022-05-13 20:56:39 -0500

    registro y clientes

--- app/src/main/java/com/example/myloginapp/Cliente.java
+++ app/src/main/java/com/example/myloginapp/Cliente.java
@@ -2,7 +2,11 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class Cliente extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class Cliente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+
+        MaterialButton register = (MaterialButton) findViewById(R.id.regis);
+
+        Intent registro = new Intent(this,RegistrarAnciano.class);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //correct
+
+                startActivity(registro);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myloginapp/RegistrarAnciano.java
@@ -0,0 +1,55 @@
+package com.example.myloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.List;
+
+public class RegistrarAnciano extends AppCompatActivity {
+
+    ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_anciano);
+
+        TextView name =(TextView) findViewById(R.id.name);
+        TextView surname =(TextView) findViewById(R.id.surname);
+        TextView gender =(TextView) findViewById(R.id.gender);
+        TextView Age =(TextView) findViewById(R.id.Age);
+        TextView document =(TextView) findViewById(R.id.document);
+        TextView address =(TextView) findViewById(R.id.address);
+        TextView illness =(TextView) findViewById(R.id.illness);
+
+        Intent registrar = new Intent(this,Registro.class);
+
+
+        MaterialButton registrarAnci = (MaterialButton) findViewById(R.id.registrarAnciano);
+
+        registrarAnci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ListElement anciano = new ListElement();
+                anciano.setName(name.getText().toString() + " "+surname.getText().toString());
+                anciano.setSexo(gender.getText().toString());
+                anciano.setEdad(Integer.parseInt(Age.getText().toString()));
+                anciano.setDocumento(document.getText().toString());
+                anciano.setDireccion(address.getText().toString());
+                anciano.setStatus(illness.getText().toString());
+                Global.elements.add(anciano);
+                Toast.makeText(RegistrarAnciano.this,"Registro Exitoso",Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+    }
+}
