2be18cff2e0a8c14bb27d47426d2589c9e19cace
danvalrl <79485807+danvalrl@users.noreply.github.com>
2022-05-14 21:19:49 -0500

    Main para doctor, admin y cliente

---
+++ app/src/main/java/com/example/myloginapp/Medico.java
@@ -0,0 +1,27 @@
+package com.example.myloginapp;
+
+public class Medico {
+
+    public String nombre;
+    public String clave;
+
+    public Medico() {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+}
