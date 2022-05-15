ecac9cd86a4faf1f3402a0ee22d7924130e10f73
LauraAguirre <lauravale06@hotmail.com>
2022-05-14 21:25:53 -0500

    Laura Aguirre

--- app/src/main/java/com/example/myloginapp/RegistrarPersonal.java
+++ app/src/main/java/com/example/myloginapp/RegistrarPersonal.java
@@ -2,7 +2,11 @@ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class RegistrarPersonal extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class RegistrarPersonal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_personal);
+
+        Intent registro = new Intent(this,RegistrarPersonal.class);
+
+        MaterialButton registrobtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        registrobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //correct
+
+            }
+        });
     }
 }
